faea1e3ccf8dbcd71137fc3c4abccb9b5b2ed646
yusei-shiraishi <yusei29game@gmail.com>
2020-04-10 02:01:21 +0000

    updated

--- src/gpio.cpp
+++ src/gpio.cpp
@@ -38,7 +38,7 @@ int Gpio::set_pin(int pin, Gpio::FunctionSelect fsel) {
 
   std::cout << "pin_num:" << pin << std::endl;
   std::cout << "addr:" << std::hex << m_addr + (pin/10) << std::endl;
-  std::cout << "val:" << ((short)fsel << 3*(pin%10)) << std::endl;
+  std::cout << "val:" << std::hex << ((short)fsel << 3*(pin%10)) << std::endl;
   std::cout << "current_val:" << std::hex << *(m_addr + (pin/10)) << std::endl;
 
   *(m_addr + (pin/10)) = (short)fsel << 3*(pin%10);
