81918e7712b81ad223097fb7493ca2466b4cdf3c
yusei-shiraishi <yusei29game@gmail.com>
2020-04-14 13:54:45 +0000

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@ int main(){
 
     if(strcmp(buff, "on") == 0){
       gpio->set_fsel(2, Gpio::FunctionSelect::IN);
-      gpio->set_pin(2);
     }
     else{
       gpio->set_fsel(2, Gpio::FunctionSelect::OUT);
