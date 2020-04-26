6785415bdf0bec0994e3c1a214b2d7df95b140fd
yusei-shiraishi <yusei29game@gmail.com>
2020-04-26 08:52:07 +0000

    updated

--- src2/gpio.cpp
+++ src2/gpio.cpp
@@ -19,32 +19,57 @@
 const int Gpio::PERIPHERAL_ADDRESS = bcm_host_get_peripheral_address();
 
 Gpio::Gpio(){
-  if ((m_fd = open(DEVICE_FILE, O_RDWR | O_SYNC)) < 0) {
+  if ((m_fd = open("/dev/mem", O_RDWR | O_SYNC)) < 0) {
     perror("failed to open()\n");
   }
 
   m_addr = (unsigned int)mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, m_fd, PERIPHERAL_ADDRESS + GPIO_OFFSET);
+  if (m_addr == MAP_FAILED) {
+    perror("failed with mmap()\n");
+    close(m_fd);
+  }
 }
 
 Gpio::~Gpio(){
   munmap((void*)m_addr, PAGE_SIZE);
   close(m_fd);
- }
+}
 
- int Gpio::set_fsel(int pin, Gpio::FunctionSelect fsel) {
-   //１つしかできないからなんとかしろ
- 
-  (*((volatile unsigned int*)((m_addr + GPFSEL0_OFFSET)))) = ((int)fsel << 3*(pin % 10));
-   return 0;
- }
+int Gpio::set_fsel(int pin, Gpio::FunctionSelect fsel) {
+  if (!validate_pin(pin)){
+    perror("gg");
+  }
 
- int Gpio::set_pin(int pin){
+  (*((volatile unsigned int*)((m_addr + GPFSEL0_OFFSET)))) = ((int)fsel << 3*pin);
+  return 0;
+}
+
+int Gpio::set_pin(int pin){
+  if (!validate_pin(pin)){
+    perror("gg");
+  }
+
   (*((volatile unsigned int*)((m_addr + GPSET0_OFFSET)))) = (1 << pin);
-   return 0;
- }
-	 
- int Gpio::clear_pin(int pin){
-  (*((volatile unsigned int*)((m_addr + GPCLR0_OFFSET)))) = (1 << pin);
-   return 0;
- }
+  return 0;
+}
 
+int Gpio::clear_pin(int pin){
+  if (!validate_pin(pin)){
+    perror("gg");
+  }
+
+  (*((volatile unsigned int*)((m_addr + GPCLR0_OFFSET)))) = (1 << pin);
+  return 0;
+}
+
+bool Gpio::validate_pin(int pin){
+  return true;
+}
+
+bool Gpio::is_high(int pin){
+  return true;
+}
+
+Gpio::FunctionSelect Gpio::get_fsel(int pin){
+  FunctionSelect::OUT;
+}
---
+++ src2/gpio.hpp
@@ -0,0 +1,32 @@
+#ifndef GPIO_HPP
+#define GPIO_HPP
+
+class Gpio{
+public:
+	Gpio();
+	~Gpio();
+
+  enum struct FunctionSelect {
+   IN  = 0b000,
+   OUT = 0b001
+  };
+
+  // Enter the GPIO pin number for the first argument 'pin'.
+  // It has nothing to do with the number on the board.
+  int set_fsel(int pin, FunctionSelect fsel);
+  int set_pin(int pin);
+  int clear_pin(int pin);
+
+private:
+	static const int PERIPHERAL_ADDRESS;
+  static const int PAGE_SIZE = 256;
+
+	int m_fd;
+	unsigned int m_addr;
+
+  bool validate_pin(int pin);
+  bool is_high(int pin);
+  FunctionSelect get_fsel(int pin);
+};
+
+#endif
