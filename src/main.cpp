fa6fc785ce78679743b207c0a5910e2d9f590377
yusei-shiraishi <yusei29game@gmail.com>
2020-05-04 07:58:14 +0000

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,39 @@
 #include <cstring>
 #include <cstdio>
 #include <unistd.h>
+#include <vector>
 #include "gpio.hpp"
 
 int main() {
   Gpio* g = new Gpio();
+  //while(true){
+  //  if(g->is_high(2)) printf("2-high\n");
+  //  printf("pls typing on or off\n");
+
+  //  char buff[256];
+  //  scanf("%s", buff);
+
+  //  if(strcmp(buff, "on") == 0){
+  //    g->set_pin(2);
+  //    g->set_pin(21);
+  //  }
+  //  else{
+  //    g->clear_pin(2);
+  //    g->clear_pin(21);
+  //  }
+  //}
+  std::vector<bool> states(256, false);
+  g->set_fsel(20, GPIO::FunctionSelect::IN);
   while(true){
-    if(g->is_high(2)) printf("2-high\n");
-    printf("pls typing on or off\n");
+    states[255] = g->is_high(20);
 
-    char buff[256];
-    scanf("%s", buff);
-
-    if(strcmp(buff, "on") == 0){
-      g->set_pin(2);
-      g->set_pin(21);
+    //print
+    std::system("cls");
+    for(auto ite = states.begin(); ite != states.end(); ++i){
+      if(*ite) printf("-");
+      else     printf("_");
     }
-    else{
-      g->clear_pin(2);
-      g->clear_pin(21);
-    }
+    printf("\n");
   }
   delete g;
 
